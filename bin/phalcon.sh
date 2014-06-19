95fcb49c06956fc7f498a14aa714796e4ecd95cf
Duy Thiện <fcduythien@gmail.com>
2014-06-19 12:37:04 +0700

    Update phalcon.sh

--- bin/phalcon.sh
+++ bin/phalcon.sh
@@ -24,6 +24,7 @@ cd $PHALCON_DIR/build
 # ./configure --enable-phalcon --with-php-config=$PHP_ROOT/bin/php-config
 # make
 # make install
+/app/.heroku/php/bin/phpize
 ./install
 cd ../..
 echo "-----> Done."
