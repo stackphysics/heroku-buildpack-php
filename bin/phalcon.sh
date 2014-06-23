e09e1706f9d046d7445fd1322c6963c5c4b6408b
stackphysics <fcopensuse@gmail.com>
2014-06-23 10:29:11 -0400

    Update phalcon.sh

--- bin/phalcon.sh
+++ bin/phalcon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build Path: /app/.heroku/php/
-
+ext_ini="$BP_DIR/conf/php/conf.d/"
 echo "$ext_ini"
 echo "important extension phalcon into php.ini"
 echo "extension=phalcon.so" >> /app/.heroku/php/etc/php/php.ini
