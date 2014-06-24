67a70d6dc10f89427fe4d8f18eae21e4d186e727
stackphysics <fcopensuse@gmail.com>
2014-06-24 13:03:59 -0400

    Update default_include.conf.php

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,7 +1,18 @@
 location / {
-    index  index.php index.html index.htm;
-}
+        root   $root_path;
+        index  index.php index.html index.htm;
 
+        # if file exists return it right away
+        if (-f $request_filename) {
+            break;
+        }
+
+        # otherwise rewrite it
+        if (!-e $request_filename) {
+            rewrite ^(.+)$ /index.php?_url=/$1 last;
+            break;
+        }
+    }
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
     deny all;
